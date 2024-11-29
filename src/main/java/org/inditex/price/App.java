fa3e5a98ab319d6e0e038bce9a52d259fc898d39
abarroso <abarroso@paradigmadigital.com>
2024-11-29 12:35:24 +0100

    first commit

---
+++ src/main/java/org/inditex/price/App.java
@@ -0,0 +1,11 @@
+package org.inditex.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
